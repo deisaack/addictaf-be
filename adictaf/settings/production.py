3af65540a13fbb7ffd42ee6f6575bed52c855087
Am Isaac <pkemey@gmail.com>
2018-06-01 18:28:02 +0300

    cache clear

--- adictaf/settings/production.py
+++ adictaf/settings/production.py
@@ -2,7 +2,6 @@ from .base import *
 
 ALLOWED_HOSTS =config('ALLOWED_HOSTS', cast=Csv())
 
-# ALLOWED_HOSTS = ['*']
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -20,3 +19,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # CORS_ORIGIN_WHITELIST = config('CORS_ORIGIN_WHITELIST', cast=Csv())
 CORS_ORIGIN_ALLOW_ALL = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+CSRF_COOKIE_SECURE = True
+ADMINS = (
+    ('Elgon Hub', 'elgonhub.com@gmail.com'),
+)
+MANAGERS = ADMINS
